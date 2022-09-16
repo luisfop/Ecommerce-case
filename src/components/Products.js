080228e390c25749f553c7b44d9c5148562ff480
luisop <luisfop77@gmail.com>
2022-09-16 11:53:50 -0300

    feat: added images to products

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,38 +3,38 @@ import Product from "./Product";
 const DUMMY_PRODUCTS = [
   {
     id: 1,
-    name: "MacBook",
+    name: "MacBook Pro",
     imgURL:
-      "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    price: 25,
+      "https://images.unsplash.com/photo-1537498425277-c283d32ef9db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1478&q=80",
+    price: 1500,
   },
   {
     id: 2,
     name: "Lenovo Yoga",
     imgURL:
-      "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    price: 25,
+      "https://www.notebookcheck.info/uploads/tx_nbc2/LenovoYoga9-14ITL5__1__01.JPG",
+    price: 1200,
   },
   {
     id: 3,
     name: "Dell lattitude",
     imgURL:
-      "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    price: 25,
+      "https://i.dell.com/is/image/DellContent//content/dam/ss2/product-images/dell-client-products/notebooks/latitude-notebooks/14-5420/media-gallery/la5420nt_cnb_00055lf110_gy_5000x5000_gettyimages-1254825733.psd?fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=660&qlt=100,0&resMode=sharp2&size=660,402",
+    price: 1800,
   },
   {
     id: 4,
     name: "HP Pavillion",
     imgURL:
-      "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    price: 25,
-  },
+      "https://www.notebookcheck.info/uploads/tx_nbc2/portatil-hp-14-al002ns-i5-6200u-4gb-1336451_Perfil_ad_l_18.png",
+    price: 1000,
+  },  
   {
     id: 5,
     name: "Acer Aspire",
     imgURL:
-      "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    price: 25,
+      "https://a-static.mlcdn.com.br/1500x1500/notebook-acer-aspire-5-intel-core-i5-8gb-256gb-ssd-156-full-hd-windows-10-a515-54-53vn/magazineluiza/233911800/e31c180b6365a232132c226e1d0e28ac.jpg",
+    price: 1250,
   },
 ];
 const Products = () => {
